1ec0bb01ecdf3235286fe09327b877b36e8690ef
Sergey Marchuk <marchuk1s1p@gmail.com>
2018-03-09 12:31:13 +0200

    create index file

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>local url shortener</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body class="text-center">
+
+    <form class='form-signin mt-5' action='index.php' method='post' style="width: 30%; min-width: 350px; margin: auto;">
+        <h1 class="h3 mb-3 font-weight-normal">Pleae sign in</h1>
+        <input class="mb-3 btn-block" type='text' placeholder='your long URL' name='url'>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <p class="mt-5 mb-3 text-muted">&copy MARICH 2018</p>
+    </form>
+<?php
+
+    error_reporting(E_ALL);
+    
+    const LENGTHPASSWORD = 10;
+    session_start();
+
+    require_once 'auth.php';
+
+?>  
+</body>
+</html>
+    
