1deb1844de2a70719137ebd463ad45c89521c9e3
Sergey Marchuk <marchuk1s1p@gmail.com>
2018-03-09 16:57:00 +0200

    add function pathToFile nad getFullUrl

--- library.php
+++ library.php
@@ -1,5 +1,15 @@
 <?php
+    
     function getShortUrl() {
+
+        do {
+            $shortUrl = generateShortUrl();
+        } while !file_exists(pathToFile($shortUrl));
+        
+        return $shortUrl
+    }
+
+    function generateShortUrl() {
         $chars = "qazxswedcvfrtgbnhyujmkiolpQAZXSWEDCVFRTGBNHYUJMKIOLP";
         $shortUrl = null;
 
@@ -8,4 +18,25 @@
         }
 
         return $shortUrl;
+    }
+
+    function pathToFile($fileName) {
+        return __DIR__.DIRECTORY_SEPARATOR."urls".DIRECTORY_SEPARATOR.$fileName;
+    }
+
+    function writeToFile($shortUrl, $longUrl) {
+
+        if (!file_exists($path)) {
+            $fp = fopen($path, "w");
+            fwrite($fp, $longUrl);
+            fclose($fp);
+        }
+    }
+
+    function getFullUrl($path) {
+        $fp = fopen($path, "r");
+        $fullUrl = file_get_contents($fp);
+        fclose($fp);
+
+        return $fullUrl
     }
